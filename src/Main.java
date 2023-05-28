ad386ceda91c75e9cff14f9d0dab60fc708bf355
“anna-kuzmiak” <kuzmiak.anna@airslate.com>
2023-05-28 12:31:33 +0300

    collections task

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,12 @@ import com.course.OOP.Manager;
 import com.course.OOP.User;
 import com.course.syntax.*;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        OOPTask();
 
-        User user = new User("Anna Kuzmiak", "annwrst@gmail.com", "Admin");
-        user.setDeliveryAddress("Ukraine", "KR", "Chornovola", "38", "03894");
-        user.printUserInfo();
-
-        User user2 = new User("Anna Kuzmak", "annwrst+88@gmail.com", "Admin");
-        user2.printUserInfo();
-
-        Manager manager1 = new Manager("Manager Name", "manaer@gv.rd", "Customer", "position");
-        manager1.printUserInfo();
-        user2.setManager(manager1);
-        manager1.setCard("5627379373630191", "12-04-2024", "622", "Visa");
-
-        System.out.println(manager1.getCards().toString());
     }
 
     public static void stepFirst() {
@@ -49,6 +39,22 @@ public class Main {
         System.out.println(StepFourth.factorial(6));
         System.out.println(StepFourth.divisor(6));
         StepFourth.getAge();
+    }
+
+    public static void OOPTask() {
+        User user = new User("Anna Kuzmiak", "annwrst@gmail.com", "Admin");
+        user.setDeliveryAddress("Ukraine", "KR", "Chornovola", "38", "03894");
+        user.printUserInfo();
+
+        User user2 = new User("Anna Kuzmak", "annwrst+88@gmail.com", "Admin");
+        user2.printUserInfo();
+
+        Manager manager1 = new Manager("Manager Name", "manaer@gv.rd", "Customer", "position");
+        manager1.printUserInfo();
+        user2.setManager(manager1);
+        manager1.setCard("5627379373630191", "12-04-2024", "622", "Visa");
+
+        System.out.println(manager1.getCards().toString());
     }
 }
 
---
+++ src/com/course/CollectionsStep.java
@@ -0,0 +1,69 @@
+package com.course;
+
+import com.course.OOP.Manager;
+import com.course.OOP.User;
+
+import java.util.*;
+
+public class CollectionsStep {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value1");
+        map.put("keyy", "value2");
+        map.put("tea", "value3");
+        System.out.println(task4(map));
+
+    }
+
+    public static void task1() {
+        Set<String> elements = new TreeSet<>();
+        elements.add("test");
+        elements.add("abc elements");
+        System.out.println(elements);
+    }
+
+    public static void task2() {
+        User user = new User("Anna Kuzmiak", "annwrt@gmail.com", "Admin");
+        User user2 = new User("Anna Test", "anwrst+88@gmail.com", "Admin");
+        User user3 = new User("Anna 3", "ann3@gmail.com", "Customer");
+        User user4 = new User("Anna 4", "ann4@gmail.com", "Main Customer");
+
+        HashMap<Integer, User> users = new HashMap<>();
+        users.put(user.getId(), user);
+        users.put(user2.getId(), user2);
+        users.put(user3.getId(), user3);
+        users.put(user4.getId(), user4);
+
+        users.forEach(((id, userElement) -> {
+            if (id % 2 == 0) userElement.printUserInfo();
+        }));
+    }
+
+    public static void task3() {
+        Set<String> namesSet = new HashSet<>();
+        namesSet.add("Olya");
+        namesSet.add("Natasha");
+        namesSet.add("Polina");
+        namesSet.add("Vlad");
+        ArrayList<String> namesList = new ArrayList<>();
+        namesList.add("Bohdan");
+        namesList.add("Oleksandr");
+        namesList.add("Vlad");
+        namesList.add("Natasha");
+        Set<String> resultSet = new HashSet<>();
+        resultSet.addAll(namesSet);
+        resultSet.addAll(namesList);
+        System.out.println(resultSet);
+    }
+
+    public static Map<String, String> task4(Map<String, String> map) {
+        Map<String, String> result = new HashMap<>();
+        map.forEach((k, v) -> {
+            if (k.length() == 3) result.put(k, v);
+        });
+        return result;
+    }
+}
